f71fbabf4d1717848ce27db5b8a72c43f1100bdc
zab3 <xelvis03@gmail.com>
2020-11-11 19:11:24 +0900

    ActionCableを利用本番環境で利用できるよう再設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://13.114.91.191/cable'
   config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
