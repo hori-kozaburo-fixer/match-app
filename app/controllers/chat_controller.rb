67aeb8e15f1c119ccaff5c644461eac8271f6bed
zab3 <xelvis03@gmail.com>
2020-11-11 14:49:50 +0900

    非同期メッセージサーバー側作成

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,4 @@
 class ChatController < ApplicationController
-  class ChatController < ApplicationController
     def create
       current_user_rooms = RoomUser.where(user_id: current_user.id).map(&:room)
       room = RoomUser.where(room: current_user_rooms, user_id: params[:user_id]).map(&:room).first
@@ -8,8 +7,6 @@ class ChatController < ApplicationController
         RoomUser.create(room: room, user_id: current_user.id)
         RoomUser.create(room: room, user_id: params[:user_id])
       end
-      redirect_to controller: :message,action: :show, id: room.id
+      redirect_to controller: :message, action: :show, id: room.id
     end
-  
-  end
 end
