5182edc6023eb8ea0c110a339940228e6e7ff615
zab3 <xelvis03@gmail.com>
2020-11-16 22:36:42 +0900

    userモデルとtweetモデルの単体テスト

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :tweet do
+    name    { Faker::Lorem.sentence } 
+    text    { Faker::Lorem.sentences }
+    team_id { 1 }
+    image {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon.png'))}
+    association :user
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tweet = FactoryBot.build(:tweet)
+  end
+
+  describe '投稿機能' do
+
+    it '全て正しい入力をすることで登録できること' do
+      expect(@tweet).to be_valid
+    end
+    
+    it '投稿画像が空だと登録できない' do
+      @tweet.image = nil
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("画像を入力してください")
+    end
+
+    it 'テーマが空だと登録できない' do
+      @tweet.name = ""
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("テーマを入力してください")
+    end
+
+    it 'テーマは30文字以内でないと登録できない' do
+      @tweet.name = "あ"*31
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("テーマは30文字以内で入力してください")
+    end
+     
+    it 'ニュースが空だと登録できない' do
+      @tweet.text = ""
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("ニュースを入力してください")
+    end
+
+    it 'ニュースは191文字以内でないと登録できない' do
+      @tweet.text = "あ"*192
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("ニュースは191文字以内で入力してください")
+    end
+
+    it 'テームを選択しないと登録できないこと' do
+      @tweet.team_id = ""
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("どのチームのニュース？を入力してください")
+    end
+  end
 end
