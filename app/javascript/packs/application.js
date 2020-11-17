7b8ace356403026f6974b942acb31029085c7f68
zab3 <xelvis03@gmail.com>
2020-11-17 13:05:15 +0900

    loading画面を表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require("swipe.js")
 require("transform.js")
 require("follow.js")
 require("countdown.js")
-
+require("load.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
