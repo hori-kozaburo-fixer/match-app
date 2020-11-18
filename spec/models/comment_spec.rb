f393ffd309f5f7d752e3d107ebecd84ae7554019
zab3 <xelvis03@gmail.com>
2020-11-18 22:42:06 +0900

    commentモデルの単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  describe 'コメント機能' do
+    it '正しく入力ができていれば登録できる' do
+      expect(@comment).to be_valid
+    end
+     
+    it 'コメントが空だと登録されない' do
+      @comment.text = ""
+      @comment.valid?
+      #ブラウザ上では非表示
+      expect(@comment.errors.full_messages).to include("Textを入力してください")
+    end
+
+    it 'コメントが192文字以上だと登録できない' do
+      @comment.text = "あ"*192
+      @comment.valid?
+      #ブラウザ上では非表示
+      expect(@comment.errors.full_messages).to include("Textは191文字以内で入力してください")
+    end
+  end
 end
