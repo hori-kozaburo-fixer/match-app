7c0a89f8ba662c7070aec043cc2140b170ecd717
zab3 <xelvis03@gmail.com>
2020-11-18 22:57:47 +0900

    ログイン機能の結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -66,3 +66,36 @@ RSpec.describe "ユーザー新規登録", type: :system do
     expect(current_path).to eq "/users"
   end
 end
+
+  RSpec.describe 'ログイン', type: :system do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+    context 'ログインができるとき' do
+      it '保存されているユーザーの情報と合致すればログインができる' do
+        visit root_path
+        find('a[href="/home/seek"]').click
+        find('a[href="/users"]').click
+        expect(page).to have_content('ログイン')
+        fill_in 'メールアドレス', with: @user.email
+        fill_in 'パスワード', with: @user.password
+        # ログインボタンを押す
+        find('input[name="commit"]').click
+        # トップページへ遷移することを確認す
+        expect(page).to have_content('ログアウト') 
+        expect(page).to have_no_content('新規登録')
+        expect(page).to have_no_content('ログイン')
+      end
+    end
+    context 'ログインができないとき' do
+      it '保存されているユーザーの情報と合致しないとログインができない' do
+        visit root_path
+        find('a[href="/home/seek"]').click
+        find('a[href="/users"]').click
+        fill_in 'メールアドレス', with: ""
+        fill_in 'パスワード', with: ""
+        find('input[name="commit"]').click
+        expect(current_path).to eq new_user_session_path
+      end
+    end
+end
