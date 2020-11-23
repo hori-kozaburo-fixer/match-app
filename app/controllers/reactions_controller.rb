71fe39ca01f158fe7051b1877a487f154484e100
zab3 <xelvis03@gmail.com>
2020-11-24 08:16:05 +0900

    不要記述修正

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,7 +1,7 @@
 class ReactionsController < ApplicationController
   def create
     reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: current_user.id)
-    reaction.update_attributes(status: params[:reaction])
+    reaction.update(status: params[:reaction])
     reaction.create_notification_follow!(current_user)
   end
 end
