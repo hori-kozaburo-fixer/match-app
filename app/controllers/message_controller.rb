a7cce9b9a17792688d1bd6723eb75bd84ea5fdeb
zab3 <xelvis03@gmail.com>
2020-11-23 21:25:03 +0900

    paramsをユーザーからroom_idに変更

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,14 +1,14 @@
 class MessageController < ApplicationController
   before_action :authenticate_user!
   def show
-    room = Room.find_by(id: params[:id])
-    @room_user = room.room_users.where.not(user_id: current_user.id).first.user
-    @messages = Message.where(room: room).order(:created_at)
+    @room = Room.find_by(id: params[:id])
+    @room_user = @room.room_users.where.not(user_id: current_user.id).first.user
+    @messages = Message.where(room: @room).order(:created_at)
     @message = Message.new
   end
 
   def create
-    room_user = RoomUser.find_by(user_id: params[:format])
+    room_user = RoomUser.find_by(room_id: params[:format])
     @message = Message.new(message: params[:message][:message], room_id: room_user.room_id, user_id: current_user.id)
     @from_user = User.find_by(id: current_user.id)
     @to_user = User.find_by(id: room_user.user_id)
