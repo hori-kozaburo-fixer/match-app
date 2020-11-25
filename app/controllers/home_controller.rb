141281255a2f42e3bfd0fdc002af482999c43692
zab3 <xelvis03@gmail.com>
2020-11-25 22:57:18 +0900

    ホームページより最終ニュースのタイトルを取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    agent = Mechanize.new
+    page = agent.get("https://basketballking.jp/news/category/bl")
+    @elements = page.search('.news-category-list__vertical__title')
   end
 
   def chat
